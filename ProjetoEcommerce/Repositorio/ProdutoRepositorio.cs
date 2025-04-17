921099d7144545474a9848e5024216a24471693d
Debora <ds336756@gmail.com>
2025-04-17 01:53:04 -0300

    Adicionando CadastrarProduto no ProdutoRepositorio

--- ProjetoEcommerce/Repositorio/ProdutoRepositorio.cs
+++ ProjetoEcommerce/Repositorio/ProdutoRepositorio.cs
@@ -4,8 +4,25 @@ using System.Data;
 
 namespace ProjetoEcommerce.Repositorio
 {
-    public class ProdutoRepositorio (IConfiguration configuration)
+    public class ProdutoRepositorio(IConfiguration configuration)
     {
         private readonly string _conexaoMySQL = configuration.GetConnectionString("ConexaoMySQL");
+
+        public void CadastrarProduto(Produto produto)
+        {
+            using (var conexao = new MySqlConnection(_conexaoMySQL))
+            {
+                conexao.Open();
+
+                MySqlCommand cmd = new MySqlCommand("Insert into Produto (Nome, Descricao, Quantidade, Preco) values (@nome, @descricao, @quantidade, @preco)", conexao);
+                cmd.Parameters.Add("@nome", MySqlDbType.VarChar).Value = produto.Nome;
+                cmd.Parameters.Add("@descricao", MySqlDbType.VarChar).Value = produto.Descricao;
+                cmd.Parameters.Add("@quantidade", MySqlDbType.Int32).Value = produto.Quantidade;
+                cmd.Parameters.Add("@preco", MySqlDbType.Decimal).Value = produto.Preco;
+
+                cmd.ExecuteNonQuery();
+                conexao.Close();
+            }
+        }
     }
 }
